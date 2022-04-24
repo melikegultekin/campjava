aa171204030e6d50d5abfe7a560269814bba8530
gmel <gultekin.melike784@gmail.com>
2022-04-25 00:53:03 +0300

    ...

--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -5,9 +5,28 @@ import java.util.List;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAllShorted();
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
 	Result add(Product product);
 
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategoryId(String productName,int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName,int categoryId);
+
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartWith(String productName);
+	
+	
+	DataResult<List<Product>> getByNameAndCategory(String productName,int categoryId);
+
+	DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails();
 }
